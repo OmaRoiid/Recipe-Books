e06db59e05d391101e523cb61673167103a64a3f
omaroiid <omarsalem977@gmail.com>
2020-12-17 01:38:25 +0200

    Add auth guard
    the guard will not allow to the user to navigate to the home page if dose not signin

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { LaptopEditComponent } from './laptops/laptop-edit/laptop-edit.component';
 import { laptopsDetailComponent } from './laptops/laptops-detail/laptops-detail.component';
 import { LaptopsStateComponent } from './laptops/laptops-state/laptops-state.component';
@@ -11,6 +12,7 @@ const appRoute: Routes = [
   {
     path: 'laptops',
     component: laptopsComponent,
+    canActivate:[AuthGuard],
     children: [
       { path: '', component: LaptopsStateComponent },
       { path: 'new', component: LaptopEditComponent },
